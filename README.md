cc12af34dd84db5d71e392a3271c87f4d77b9eb5
Wandalen <wandalen.me@gmail.com>
2019-08-05 13:34:08 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# wResolver [![Build Status](https://travis-ci.org/Wandalen/wResolver.svg?branch=master)](https://travis-ci.org/Wandalen/wResolver)
+
+Collection of routines to resolve complex data structures. It takes a complex data structure, traverses it and resolves all strings having inlined special substrings. Use the module to resolve your templates.
+
+## Try out
+```
+npm install
+node sample/Sample.s
+```
---
+++ proto/dwtools/abase/l6.test/Resolver.test.s
@@ -0,0 +1,232 @@
+( function _Resolver_test_s_() {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  let _ = require( '../../Tools.s' );
+
+  _.include( 'wTesting' );
+  _.include( 'wLogger' );
+
+  require( '../l7/Resolver.s' );
+
+}
+
+var _global = _global_;
+var _ = _global_.wTools;
+
+// --
+// tests
+// --
+
+function selectorParse( test )
+{
+  let self = this;
+  let r = _.Resolver;
+
+  test.case = 'single inline, single split';
+  var expected =
+  [
+    [
+      [ "a", "::", "b" ]
+    ]
+  ]
+  var got = r.selectorParse( '{a::b}' );
+  test.identical( got, expected );
+
+  test.case = 'implicit inline, single split';
+  var expected =
+  [
+    [
+      [ "a", "::", "b" ]
+    ]
+  ]
+  var got = r.selectorParse( 'a::b' );
+  test.identical( got, expected );
+
+  test.case = 'single inline, several splits';
+  var expected =
+  [
+    [
+      [ 'a', '::', 'b' ],
+      [ 'c', '::', 'd' ]
+    ],
+  ]
+  var got = r.selectorParse( '{a::b/c::d}' );
+  test.identical( got, expected );
+
+  test.case = 'implicit inline, several splits';
+  var expected =
+  [
+    [
+      [ 'a', '::', 'b' ],
+      [ 'c', '::', 'd' ]
+    ],
+  ]
+  var got = r.selectorParse( 'a::b/c::d' );
+  test.identical( got, expected );
+
+  test.case = 'single inline, several splits, non-selector sides';
+  var expected =
+  [
+    'x',
+    [
+      [ 'a', '::', 'b' ],
+      [ 'c', '::', 'd' ]
+    ],
+    'y'
+  ]
+  var got = r.selectorParse( 'x{a::b/c::d}y' );
+  test.identical( got, expected );
+
+  test.case = 'several inlines';
+  var expected =
+  [
+    "x",
+    [
+      [ "a", "::", "b" ],
+      [ "c", "::", "d" ]
+    ],
+    "y",
+    [
+      [ "ee", "::", "ff" ],
+      [ "gg", "::", "hhh" ]
+    ],
+    "z",
+  ]
+  var got = r.selectorParse( 'x{a::b/c::d}y{ee::ff/gg::hhh}z' );
+  test.identical( got, expected );
+
+  test.case = 'several inlines, split without ::';
+  var expected =
+  [
+    "x",
+    [
+      [ "a", "::", "b" ],
+      [ "", "", "mid" ],
+      [ "c", "::", "d" ]
+    ],
+    "y",
+    [
+      [ "ee", "::", "ff" ],
+      [ "gg", "::", "hhh" ]
+    ],
+    "z",
+  ]
+  var got = r.selectorParse( 'x{a::b/mid/c::d}y{ee::ff/gg::hhh}z' );
+  test.identical( got, expected );
+
+  test.case = 'critical, no ::';
+  var expected =
+  [
+    'x{mid}y{}z',
+  ]
+  var got = r.selectorParse( 'x{mid}y{}z' );
+  test.identical( got, expected );
+
+  test.case = 'critical, empty side';
+  var expected =
+  [
+    'x',
+    [
+      [ 'aa', '::', '' ]
+    ],
+    'y',
+    [
+      [ '', '::', 'bb' ]
+    ],
+    'z',
+    [
+      [ '', '::', '' ]
+    ]
+  ]
+  var got = r.selectorParse( 'x{aa::}y{::bb}z{::}' );
+  test.identical( got, expected );
+
+}
+
+//
+
+function selectorNormalize( test )
+{
+  let self = this;
+  let r = _.Resolver;
+
+  test.case = 'single inline, single split';
+  var expected = '{a::b}';
+  var got = r.selectorNormalize( '{a::b}' );
+  test.identical( got, expected );
+
+  test.case = 'implicit inline, single split';
+  var expected = '{a::b}'
+  var got = r.selectorNormalize( 'a::b' );
+  test.identical( got, expected );
+
+  test.case = 'single inline, several splits';
+  var expected = '{a::b/c::d}';
+  var got = r.selectorNormalize( '{a::b/c::d}' );
+  test.identical( got, expected );
+
+  test.case = 'implicit inline, several splits';
+  var expected = '{a::b/c::d}';
+  var got = r.selectorNormalize( 'a::b/c::d' );
+  test.identical( got, expected );
+
+  test.case = 'single inline, several splits, non-selector sides';
+  var expected = 'x{a::b/c::d}y';
+  var got = r.selectorNormalize( 'x{a::b/c::d}y' );
+  test.identical( got, expected );
+
+  test.case = 'several inlines';
+  var expected = 'x{a::b/c::d}y{ee::ff/gg::hhh}z';
+  var got = r.selectorNormalize( 'x{a::b/c::d}y{ee::ff/gg::hhh}z' );
+  test.identical( got, expected );
+
+  test.case = 'several inlines, split without ::';
+  var expected = 'x{a::b/mid/c::d}y{ee::ff/gg::hhh}z';
+  var got = r.selectorNormalize( 'x{a::b/mid/c::d}y{ee::ff/gg::hhh}z' );
+  test.identical( got, expected );
+
+  test.case = 'critical, no ::';
+  var expected = 'x{mid}y{}z';
+  var got = r.selectorNormalize( 'x{mid}y{}z' );
+  test.identical( got, expected );
+
+  test.case = 'critical, empty side';
+  var expected = 'x{aa::}y{::bb}z{::}';
+  var got = r.selectorNormalize( 'x{aa::}y{::bb}z{::}' );
+  test.identical( got, expected );
+
+}
+
+// --
+// declare
+// --
+
+var Self =
+{
+
+  name : 'Tools/base/l7/Resolver',
+  silencing : 1,
+
+  context :
+  {
+  },
+
+  tests :
+  {
+
+    selectorParse,
+    selectorNormalize,
+
+  }
+
+}
+
+Self = wTestSuite( Self );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self.name );
+
+})();
