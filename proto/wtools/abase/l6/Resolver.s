a4ccbddd2a71b02393649b388c46669d1f7628e2
wandalen <wandalen.me@gmail.com>
2021-04-30 09:41:15 +0300

    .

--- proto/wtools/abase/l6/Resolver.s
+++ proto/wtools/abase/l6/Resolver.s
@@ -500,7 +500,7 @@ function errResolvingMake( o )
   if( o.err && o.err.ResolvingError )
   return o.err;
 
-  o.err = it.errMake( 'Failed to resolve', _.ct.format( _.entity.exportStringShallow( o.selector ), 'path' ), '\n', o.err );
+  o.err = it.errMake( 'Failed to resolve', _.ct.format( _.entity.exportStringDiagnosticShallow( o.selector ), 'path' ), '\n', o.err );
   _.error.concealedSet( o.err, { ResolvingError : true } );
 
   return o.err;
