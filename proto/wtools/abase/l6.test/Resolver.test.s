dbc9b9bcdb13ebda51042ac8dbd58b4eab34adb1
dmvict <dm.vict.kr@gmail.com>
2021-06-10 11:06:35 +0300

    Improve test suite `Resolver.test.s`, replace extra dependency

--- proto/wtools/abase/l6.test/Resolver.test.s
+++ proto/wtools/abase/l6.test/Resolver.test.s
@@ -8,7 +8,8 @@ if( typeof module !== 'undefined' )
   const _ = require( '../../../node_modules/Tools' );
 
   _.include( 'wTesting' );
-  _.include( 'wLogger' );
+  // _.include( 'wLogger' );
+  _.include( 'wStringer' );
 
   require( '../l6/Resolver.s' );
 }
